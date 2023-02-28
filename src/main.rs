2f339004ccb286d00ea2843815965fe3e40971e8
Jacob Schwarz <jacob.schwarz@gmx.de>
2023-02-28 21:40:04 +0100

    modules are fun

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,96 @@
-use std::path::Path;
-use log::{error, info, debug};
-use simple_logger::SimpleLogger;
-use sha256::try_digest;
-use sqlx::SqliteExecutor;
-use sqlx::sqlite::SqlitePool;
-use async_recursion::async_recursion;
-use std::env;
+mod model {
+    #[derive(Debug)]
+    pub struct Photo {
+        id: i64,
 
-#[derive(Debug)]
-struct Photo {
-    id: i64,
-    path: String,
-    hash: String,
-}
+        pub path: String,
+        pub hash: String,
+    }
 
-#[async_recursion]
-async fn get_photos_in_path(path: &Path) -> Vec<Photo> {
-    let mut photos: Vec<Photo> = Vec::new();
-
-    debug!("Getting photos in path: {:?}", path);
-
-    if let Ok(entries) = std::fs::read_dir(path) {
-        for entry in entries {
-
-            if let Ok(entry) = entry {
-                let i_path = entry.path();
-
-                if i_path.is_file() {
-                    photos.push(Photo {
-                        id: 0,
-                        path: i_path.to_string_lossy().to_string(),
-                        hash: try_digest(entry.path().as_path()).unwrap()
-                    });
-                } else if i_path.is_dir() {
-                    let sub_dir_photos = get_photos_in_path(i_path.as_path()).await;
-                    photos.extend(sub_dir_photos);
-                }
+    impl Photo {
+        pub fn new(id: i64, path: String, hash: String) -> Self {
+            Self {
+                id,
+                path,
+                hash,
             }
         }
     }
-
-    debug!("Found photos in path {:?}: {:?}", path, photos);
-
-    photos
 }
 
-async fn insert_photo(photo: &Photo, pool: &SqlitePool) -> anyhow::Result<()> {
-    sqlx::query!("INSERT INTO photos (path, hash) VALUES (?1, ?2)", photo.path, photo.hash).execute(pool).await?;
+mod index {
+    use std::path::Path;
+    use sqlx::SqliteExecutor;
+    use async_recursion::async_recursion;
+    use log::debug;
+    use sqlx::sqlite::SqlitePool;
+    use sha256::try_digest;
 
-    Ok(())
-}
+    pub async fn create_index(path: String, pool: &SqlitePool) -> anyhow::Result<()> {
 
-async fn create_index(path: String, pool: &SqlitePool) -> anyhow::Result<()> {
+        let photos = get_photos_in_path(Path::new(&path)).await;
 
-    let photos = get_photos_in_path(Path::new(&path)).await;
+        println!("{:?}", photos);
 
-    println!("{:?}", photos);
+        for photo in photos {
+            insert_photo(&photo, pool).await?;
+        }
 
-    for photo in photos {
-        insert_photo(&photo, pool).await?;
+        Ok(())
     }
 
-    Ok(())
-}
+    pub async fn list_index(pool: &SqlitePool) -> anyhow::Result<()> {
 
-async fn list_index(pool: &SqlitePool) -> anyhow::Result<()> {
+        let records = sqlx::query!("SELECT id, path, hash FROM photos").fetch_all(pool).await?;
 
-    let records = sqlx::query!("SELECT id, path, hash FROM photos").fetch_all(pool).await?;
+        for record in records {
+            println!("{:?}", super::model::Photo::new (
+                record.id,
+                record.path,
+                record.hash,
+            ));
+        };
 
-    for record in records {
-        println!("{:?}", Photo {
-            id:     record.id,
-            path:   record.path,
-            hash:   record.hash,
-        });
-    };
+        Ok(())
+    }
 
-    Ok(())
+    #[async_recursion]
+    async fn get_photos_in_path(path: &Path) -> Vec<super::model::Photo> {
+        let mut photos: Vec<super::model::Photo> = Vec::new();
+
+        debug!("Getting photos in path: {:?}", path);
+
+        if let Ok(entries) = std::fs::read_dir(path) {
+            for entry in entries {
+
+                if let Ok(entry) = entry {
+                    let i_path = entry.path();
+
+                    if i_path.is_file() {
+                        photos.push(super::model::Photo::new (                       
+                            0,
+                            i_path.to_string_lossy().to_string(),
+                            try_digest(entry.path().as_path()).unwrap()
+                        ));
+                    } else if i_path.is_dir() {
+                        let sub_dir_photos = get_photos_in_path(i_path.as_path()).await;
+                        photos.extend(sub_dir_photos);
+                    }
+                }
+            }
+        }
+
+        debug!("Found photos in path {:?}: {:?}", path, photos);
+
+        photos
+    }
+
+    async fn insert_photo(photo: &super::model::Photo, pool: &SqlitePool) -> anyhow::Result<()> {
+        sqlx::query!("INSERT INTO photos (path, hash) VALUES (?1, ?2)", photo.path, photo.hash).execute(pool).await?;
+
+        Ok(())
+    }
+
 }
 
 mod cli {
@@ -109,19 +123,25 @@ mod cli {
                 match index.subcommand() {
                     Some(("create", create)) => {
                         let path = create.get_one::<String>("PATH").expect("required");            
-                        super::create_index(path.to_string(), pool).await?;
+                        super::index::create_index(path.to_string(), pool).await?;
                     },
                     Some(("list", _)) => {
-                        super::list_index(pool).await?;
+                        super::index::list_index(pool).await?;
                     },
                     _ => (),
                 }
             },
             _ => (),
         };
+
         Ok(())
     }
 }
+
+use simple_logger::SimpleLogger;
+use sqlx::sqlite::SqlitePool;
+use std::env;
+
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
